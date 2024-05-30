fc0264dd8201e04c89fd3c56894640a9234f8849
YairValladares <yair.valladares23@itca.edu.sv>
2024-05-29 23:50:32 -0600

    arreglos actualizados

--- controller/ColorController.php
+++ controller/ColorController.php
@@ -11,21 +11,21 @@
         public function ActualizarColor()
         {
             require_once 'view/pages/include/header_principal.php';
-            require_once 'view/pages/Cliente/ActualizarColor.php';
+            require_once 'view/pages/Color/ActualizarColor.php';
             require_once 'view/pages/include/footer.php';
         }
 
         public function CrearColor()
         {
             require_once 'view/pages/include/header_principal.php';
-            require_once 'view/pages/Cliente/CrearColor.php';
+            require_once 'view/pages/Color/CrearColor.php';
             require_once 'view/pages/include/footer.php';
         }
 
         public function EliminarColor()
         {
             require_once 'view/pages/include/header_principal.php';
-            require_once 'view/pages/Cliente/EliminarColor.php';
+            require_once 'view/pages/Color/EliminarColor.php';
             require_once 'view/pages/include/footer.php';
         }        
     }
