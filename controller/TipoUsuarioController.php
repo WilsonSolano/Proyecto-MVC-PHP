ef84a522a46a58f25afc99721cf24d9521f7ecba
Orisama03 <112028499+Orisama03@users.noreply.github.com>
2024-05-29 20:30:06 -0600

    Agregamos los controladores para talla, tallacliente, usuario y tipousuario

---
+++ controller/TipoUsuarioController.php
@@ -0,0 +1,11 @@
+<?php
+    class TallaController
+    {
+        public function Index()
+        {
+            require_once 'view/pages/include/header_principal.php';
+            require_once 'view/pages/leer_tipoUsuario.php';
+            require_once 'view/pages/include/footer.php';
+        }
+    }
+?>
