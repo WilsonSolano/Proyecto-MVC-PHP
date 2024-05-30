d532f02a6e7ca161f367f01157e717ac687266b1
YairValladares <yair.valladares23@itca.edu.sv>
2024-05-29 18:27:40 -0600

    Se agrega el proyecto

--- view/pages/Crear/insertar_talla.php
+++
@@ -1,7 +0,0 @@
-        <h3 class="display-7 text-center mb-4 mt-3">Registrar una Talla</h3>
-
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">Nombre: </span>
-            <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
-            <input class="btn btn-primary" type="submit" value="Insertar">
-        </div>  
