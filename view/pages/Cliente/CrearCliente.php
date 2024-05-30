c62dcef8edeb5ee5b7a474dede4a9ebc245700cf
YairValladares <yair.valladares23@itca.edu.sv>
2024-05-29 22:52:54 -0600

    va con el anterior

--- view/pages/Cliente/CrearCliente.php
+++ view/pages/Cliente/CrearCliente.php
@@ -1,14 +1,28 @@
-        <div class="container text-center mt-3">
-            <h1 class="display-4">PROYECTO FINAL - DESARROLLO WEB</h1>
-            <h2 class="display-6">---------CREAR CLIENTE---------</h2>
-        </div>    
-    </header>
-    <main class="container ml-1 mr-5 mt-4 border border-success">
-        <h3 class="display-7 text-center mb-4 mt-3">Registrar una Cliente</h3>
+<div class="container">
+            <h3 class="display-7 text-center mb-4 mt-3">Registrar un Cliente</h3>
 
-        <div class="input-group mb-5">
-            <span class="input-group-text" id="basic-addon1">Nombre: </span>
-            <input type="text" class="form-control" placeholder="Nombre" aria-label="Nombre" aria-describedby="basic-addon1">
-            <input class="btn btn-primary" type="submit" value="Enviar">
-        </div>
-    </main>
+            <div class="input-group mb-2">
+                <span class="input-group-text" id="basic-addon1">Nombres:</span>
+                <input type="text" class="form-control" placeholder="Victor Hilario" aria-label="Nombre" aria-describedby="basic-addon1">
+            </div>
+            <div class="input-group mb-2">
+                <span class="input-group-text" id="basic-addon1">Apellidos:</span>
+                <input type="text" class="form-control" placeholder="Solano Durán" aria-label="Nombre" aria-describedby="basic-addon1">
+            </div>
+            <div class="mb-3">
+                <label for="exampleFormControlInput1" class="form-label">Email:</label>
+                <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+            </div>
+            <div class="input-group mb-2">
+                <span class="input-group-text" id="basic-addon1">Telefono:</span>
+                <input type="text" class="form-control" placeholder="1234-5678" aria-label="Nombre" aria-describedby="basic-addon1">
+            </div>
+
+            <select class="form-select" aria-label="Default select example">
+                <option value selected disabled>Tipo de Usuario</option>
+                <option value="1">Pro</option>
+                <option value="0">No pro</option>
+            </select>
+
+            <input class="btn btn-primary mt-3" type="submit" value="Insertar">
+</div>
