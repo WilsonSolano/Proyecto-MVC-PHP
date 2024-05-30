ba7075cbfaa9a75e5eacd689a07bdde8d04b7383
YairValladares <yair.valladares23@itca.edu.sv>
2024-05-29 18:46:59 -0600

    cambios en logincontroller

--- controller/LoginController.php
+++ controller/LoginController.php
@@ -1,15 +1,40 @@
 <?php
-    class LoginController
-    {
-        public function Entrar(){
-            $usuario = new usuario();
+    //para usar los métodos del modelo usuario
+    require_once 'model/Usuario.php';
 
+    class LoginController{
+    
+        private $model;
+        
+        public function __CONSTRUCT(){
+            //inicializa el modelo
+            $this->model = new Usuario();
+        }
+            
+        public function Index(){
+            //llama la vista login
+            require_once 'view/pages/include/header_login.php';
+            require_once 'view/login.php';
+            require_once 'view/pages/include/footer_login.php'; 
+            
+        }
+    
+        public function Entrar(){
+            $usuario = new Usuario();
+            
             //captura todos los datos
             $email = $_REQUEST['email'];
-            $Contraseña = $_REQUEST['Contraseña'];
-
-            //Consultar los datos
-            $usuario = $this->model->Entrar($email,$Contraseña);
-        }
+            $clave = $_REQUEST['clave'];
+    
+            //consultar los datos
+            $usuario = $this->model->Entrar($email, $clave);
+    
+            //iniciar la sesión o retornar al login
+            $this->model->GenerarSesion($usuario);              
+           
+        } 
+       
     }
+
+
 ?>
