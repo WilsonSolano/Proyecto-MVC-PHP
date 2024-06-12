b82525c60e01439278ee707365f1dcb6f6861961
YairValladares <yair.valladares23@itca.edu.sv>
2024-06-12 13:00:05 -0600

    conexion root

--- model/Conexion.php
+++ model/Conexion.php
@@ -2,7 +2,7 @@
     class Conexion
     {
         public static function Conectar(){
-            $pdo = new PDO('mysql:host=localhost;dbname=clothing_store_bd;charset=utf8','root','');
+            $pdo = new PDO('mysql:host=localhost;dbname=clothing_store_db;charset=utf8','root','');
             $pdo->setAttribute(PDO::ATTR_ERRMODE,PDO::ERRMODE_EXCEPTION);
 
             return $pdo;
