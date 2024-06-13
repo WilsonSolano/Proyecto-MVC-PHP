ec94bc439f0790ad329bd09129e6c163f71c2b7f
YairValladares <yair.valladares23@itca.edu.sv>
2024-06-12 23:42:17 -0600

    agregacion de cartas en principal

--- controller/ColorFavoritoController.php
+++ controller/ColorFavoritoController.php
@@ -1,6 +1,6 @@
 <?php
 
-    require_once 'model/ColorFavoritoController.php';
+    require_once 'model/ColoFavorito.php';
 
     class ColorFavoritoController
     {
--- view/principal.php
+++ view/principal.php
@@ -16,56 +16,97 @@
           <h5 class="card-title">Selecciones una operación</h5>
           <br>
           
-          <div class="row">
-            <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                  <a class="icon-link text-success" href="?c=<?php echo base64_encode('Cliente'); ?>">
-                    <i class="bi-person-arms-up text-success" style="font-size: 4rem;"></i>
-                    Cliente
-                </a>
+            <div class="row">
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('TallaCliente'); ?>">
+                      <i class="bi bi-universal-access text-success" style="font-size: 4.75rem;"></i>
+                      TallaClientes
+                  </a>
+                  </div>
                 </div>
               </div>
 
-            </div>
-            
-            <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                  <a class="icon-link text-success" href="?c=<?php echo base64_encode('Talla'); ?>">
-                    <i class="bi-file-earmark-font-fill text-success" style="font-size: 4rem;"></i>
-                    Talla
-                </a>
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('TipoUsuario'); ?>">
+                      <i class="bi-people-fill text-success" style="font-size: 4rem;"></i>
+                      Tipo Usuario
+                  </a>
+                  </div>
                 </div>
               </div>
 
-            </div>
-
-            <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                  <a class="icon-link text-success" href="?c=<?php echo base64_encode('ColorFavorito'); ?>">
-                    <i class="bi-file-earmark-font-fill text-success" style="font-size: 4rem;"></i>
-                    ColorFavorito
-                </a>
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('Usuario'); ?>">
+                      <i class="bi bi-person-circle text-success" style="font-size: 4.75rem;"></i>
+                      Usuarios
+                  </a>
+                  </div>
                 </div>
               </div>
 
-            </div>
-            
-            <div class="col-4">
-              <div class="card">
-                <div class="card-body">
-                  <a class="icon-link text-success" href="?c=<?php echo base64_encode('Color'); ?>">
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('Talla'); ?>">
+                      <i class="bi-file-earmark-font-fill text-success" style="font-size: 4rem;"></i>
+                      Talla
+                  </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('Color'); ?>">
+                      <i class="bi-palette-fill text-success" style="font-size: 4rem;"></i>
+                      Color
+                  </a>
+                  </div>
+                </div>
+              </div>
+
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('ColoFavorito'); ?>">
                     <i class="bi-palette-fill text-success" style="font-size: 4rem;"></i>
-                    Color
-                </a>
+                    ColorFavorito
+                  </a>
+                  </div>
                 </div>
               </div>
 
-            </div>
-          </div>
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('PreguntaSecreta'); ?>">
+                      <i class="bi bi-question text-success" style="font-size: 5rem;"></i>
+                      Pregunta Secreta
+                  </a>
+                  </div>
+                </div>
+              </div>
 
+              <div class="col-4">
+                <div class="card">
+                  <div class="card-body">
+                    <a class="icon-link text-success" href="?c=<?php echo base64_encode('Cliente'); ?>">
+                      <i class="bi-person-arms-up text-success" style="font-size: 3.95rem;"></i>
+                      Cliente
+                  </a>
+                  </div>
+              </div>
+
+            </div>
+           
+          </div>
         </div>
       </div>
       <br>
